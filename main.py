493617c37a9fea98231dffc5141b43bbd6950b9e
jmccallister93 <jaredmccallister93@gmail.com>
2022-06-15 11:06:38 -0700

    Updated Styling - added Save

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 root = customtkinter.CTk()
 root.title("NPC Maker")
+root.geometry("300x725")
 
 #Define details label
 details_label = []
@@ -42,35 +43,40 @@ npc_properties = ["Race",
 
 #Print NPC properties as row headers
 for index, row in enumerate(npc_properties):
-        properties_label = Label(root, text = row + ":")
-        properties_label.grid(row=index + 1, column=0, pady=10)
+        properties_label = customtkinter.CTkLabel(root, text = row + ":")
+        properties_label.grid(row=index + 2, column=0, pady=10)
         
 
 #Generate funciton
 def generate():
         
         global npc
-        if sex_choice.get() == "Male":
-                if race_choice.get() == "Dwarf":
+        sex_choice = StringVar()
+        sex_choice = sex_dropdown_menu.get()
+        race_choice = StringVar()
+        race_choice = race_dropdown_menu.get()
+
+        if  sex_choice == "Male":
+                if race_choice == "Dwarf":
                         npc = choose_dwarf_male()
-                elif race_choice.get() == "Elf":
+                elif race_choice == "Elf":
                         npc = choose_elf_male()
-                elif race_choice.get() == "Gnome":
+                elif race_choice == "Gnome":
                         npc = choose_gnome_male()
-                elif race_choice.get() == "Halfling":
+                elif race_choice == "Halfling":
                         npc = choose_halfling_male()
-                elif race_choice.get() == "Human":
+                elif race_choice == "Human":
                         npc = choose_human_male()
-        elif sex_choice.get() == "Female":
-                if race_choice.get() == "Dwarf":
+        elif sex_choice == "Female":
+                if race_choice == "Dwarf":
                         npc = choose_dwarf_female()
-                elif race_choice.get() == "Elf":
+                elif race_choice == "Elf":
                         npc = choose_elf_female()
-                elif race_choice.get() == "Gnome":
+                elif race_choice == "Gnome":
                         npc = choose_gnome_female()
-                elif race_choice.get() == "Halfling":
+                elif race_choice == "Halfling":
                         npc = choose_halfling_female()
-                elif race_choice.get() == "Human":
+                elif race_choice == "Human":
                         npc = choose_human_female()
         else:
                 npc = {}
@@ -81,24 +87,46 @@ def generate():
         details_label.clear()
 
         for index,(key, value) in enumerate(npc.items()):
-                details_label.append(Label(root, text=value, font=("Aerial 10")))
-                details_label[-1].grid(row=index+1, column=1, pady=10)
-   
-#Choose Sex of NPC
+                details_label.append(customtkinter.CTkLabel(root, text=value))
+                details_label[-1].grid(row=index+2, column=1, pady=10)
+
+        
+        
+
+#Sex/Race Options
 sex_options = ["Male", "Female"]
-sex_choice = StringVar()
-sex_choice.set("Choose Sex")
-#Choose Race of NPC
 race_options = ["Dwarf", "Elf", "Gnome", "Halfling", "Human"]
-race_choice = StringVar()
-race_choice.set("Choose Race")
-#Sex dropdown menu
-sex_dropdown_menu = OptionMenu(root, sex_choice, *sex_options)
-sex_dropdown_menu.grid(row=0, column=0, pady=15, padx=5)
-#Race dropdown menu
-race_dropdown_menu = OptionMenu(root, race_choice, *race_options).grid(row=0, column=1, pady=15, padx=5)
+
+#Sex Dropdown Menu
+sex_dropdown_menu = customtkinter.CTkComboBox(root, values=sex_options)
+sex_dropdown_menu.grid(row=1, column=0, pady=15, padx=5)
+sex_dropdown_menu.set("Choose Sex")
+
+#Race Dropdown Menu
+race_dropdown_menu = customtkinter.CTkComboBox(root, values=race_options)
+race_dropdown_menu.grid(row=1, column=1, pady=15, padx=5)
+race_dropdown_menu.set("Choose Race")
 
 #Generate NPC Button
-generate_button = Button(root, text="Generate NPC", command=generate).grid(row=0, column=2, pady=15, padx=5)
+generate_button = customtkinter.CTkButton(root, text="Generate NPC", command=generate)
+generate_button.grid(row=0, column=0, columnspan=2, pady=15, padx=5)
+
+#Save function
+def save_npc():
+        as_save=[]
+        for key,value in npc.items():
+                as_save.append(f"{key}: {value}")
+              
+        file_naming = npc["Name"]
+        file = open(f"npc_{file_naming}.txt", "w")
+        file.write(str(as_save))
+        file.close
+
+
+
+#Save NPC Button
+save_button = customtkinter.CTkButton(root, text="Save NPC", command=save_npc)
+save_button.grid(row=14, column=0, columnspan=2, pady=15, padx=5)
+
 
 root.mainloop()
